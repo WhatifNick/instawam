cb0f1ff0faf0e2b3b8582809c47fe191aa5497d0
Nick Pasquariello <npasquariello@gmail.com>
2018-03-28 16:54:04 +1000

    Git ignore upadted

--- app/models/imagepost.rb
+++ app/models/imagepost.rb
@@ -2,6 +2,7 @@ class Imagepost < ApplicationRecord
   belongs_to :user
   # has_many :image
   mount_uploader :image, ImagepostUploader
+  validates :caption, presence: true
   # mount_uploader :image, TweetImageUploader
 
 end
